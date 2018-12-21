c2c088f224d2951abc05795de282b710efaa0c11
DomTripodi93 <dtripodi93@gmail.com>
2018-12-21 00:27:51 -0500

    added initial code

---
+++ backend/backend/settings.py
@@ -0,0 +1,103 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = '4hzbTHISIAFAKEPRODUCTIONKEYpv@adai'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'propage',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'backend.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['./propage/Templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'backend.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'social4',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LOGOUT_URL = '/'
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'propage.ProUser'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
---
+++ backend/propage/views.py
@@ -0,0 +1,46 @@
+from django.http import HttpResponse
+from django.conf import settings
+
+from .models import Propage, ProUser, Posts
+from .serializers import PropageSerializer, ProUserSerializer, PostsSerializer
+from .permissions import UpdateOwnProUser, CreateOwnPost, UpdateOwnPropage
+
+from rest_framework import status, viewsets, filters, generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication 
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
+
+from .forms import UserCreationForm
+
+class PostsViewSet(viewsets.ModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    queryset = Posts.objects.all().order_by('-created_at')
+    serializer_class = PostsSerializer
+    permission_classes=(CreateOwnPost, )
+    def perform_create(self, serializer):
+        serializer.save(user = self.request.user)
+
+class PropageViewSet(viewsets.ModelViewSet):
+    queryset = Propage.objects.all().order_by('-user')
+    serializer_class = PropageSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (UpdateOwnPropage, )
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('user',)
+
+class RegisterViewSet(viewsets.ModelViewSet):
+    serializer_class = ProUserSerializer
+    queryset = ProUser.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (UpdateOwnProUser, )
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('email','name',)
+
+class LoginViewSet(viewsets.ViewSet):
+    serializer_class= AuthTokenSerializer
+
+    def create(self, request):
+        return ObtainAuthToken().post(request)
